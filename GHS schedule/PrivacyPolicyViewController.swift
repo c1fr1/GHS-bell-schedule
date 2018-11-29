7b9d14f8aa450bf8b3c61e62b1d14cd77048e00d
c1fr1 <c1fr1.stkcps@gmail.com>
2018-11-28 17:38:52 -0800

    added privacy policy

---
+++ GHS schedule/PrivacyPolicyViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PrivacyPolicyViewController.swift
+//  GHS schedule
+//
+//  Created by Varas Pendragon on 11/28/18.
+//  Copyright © 2018 4inunison. All rights reserved.
+//
+
+import UIKit
+
+class PrivacyPolicyViewController : UIViewController {
+    
+    @IBAction func close(_ : AnyObject) {
+        dismiss(animated: true) {
+        }
+    }
+}
